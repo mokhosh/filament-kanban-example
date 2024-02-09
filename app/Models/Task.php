3e5249cce50ecb85a320ca2cdf9a93562958a675
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-09 21:21:57 +0330

    add recent indication to tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
+use Mokhosh\FilamentKanban\Concerns\HasRecentUpdateIndication;
 
 class Task extends Model implements Sortable
 {
-    use HasFactory, SortableTrait;
+    use HasFactory, SortableTrait, HasRecentUpdateIndication;
 
     protected $guarded = [];
 }
