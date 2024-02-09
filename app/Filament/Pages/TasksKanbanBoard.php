543ec1eb79d21be66b05127cf68918a5c2e3ea1f
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-09 21:21:28 +0330

    update tasks kanban to v2

--- app/Filament/Pages/TasksKanbanBoard.php
+++ app/Filament/Pages/TasksKanbanBoard.php
@@ -4,29 +4,11 @@ namespace App\Filament\Pages;
 
 use App\Enums\TaskStatus;
 use App\Models\Task;
-use Illuminate\Support\Collection;
 use Mokhosh\FilamentKanban\Pages\KanbanBoard;
 
 class TasksKanbanBoard extends KanbanBoard
 {
-    protected function statuses(): Collection
-    {
-        return TaskStatus::statuses();
-    }
+    protected static string $model = Task::class;
 
-    protected function records(): Collection
-    {
-        return Task::ordered()->get();
-    }
-
-    public function onStatusChanged(int $recordId, string $status, array $fromOrderedIds, array $toOrderedIds): void
-    {
-        Task::find($recordId)->update(['status' => $status]);
-        Task::setNewOrder($toOrderedIds);
-    }
-
-    public function onSortChanged(int $recordId, string $status, array $orderedIds): void
-    {
-        Task::setNewOrder($orderedIds);
-    }
+    protected static string $statusEnum = TaskStatus::class;
 }
